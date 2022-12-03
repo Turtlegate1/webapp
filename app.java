25e163159bfc877cd242200657d816789453317f
ikenna ofole <ikennaofole@outlook.com>
2022-12-03 00:22:41 -0500

    first commit

--- app.java
+++ app.java
@@ -3,3 +3,4 @@
 # make use of variables and dynamic variables
 # Author-Ikenna Ofole
 # This file has been edited. It is well.
+# No going back, we are millionaires
--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
 database connection
 db-password=$password
 db-username=$user
-
+where are we going? no where
